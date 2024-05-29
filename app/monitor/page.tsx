f6dfc2c3c47f9ff26bc0a3599f79042e4c33b66a
Takashi Kasai <takassh23@gmail.com>
2024-05-29 23:41:19 +0900

    add metadata

--- app/monitor/page.tsx
+++ app/monitor/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from 'next';
 import { Charts } from './charts';
+
+export const metadata: Metadata = {
+  title: 'Activity Monitor',
+  icons: './favicon.ico',
+  openGraph: {
+    images: 'https://bucket.takassh.com/background.png',
+  },
+};
 
 export default function Page() {
   return <Charts />;
